791ea9a4c8bf2dfc208d75353a754f45e5581b8d
Terrel bennett <terrelbennett@yahoo.com>
2023-08-17 16:24:57 -0400

    Add user authentication feature
    This commit adds user authentication using Flask-Security. User registration,
    login, and password reset functionalities have been implemented.
    
    Fixes #123

---
+++ Chatbot/script.js
@@ -0,0 +1,307 @@
+// Get references to HTML elements
+const chatArea = document.getElementById("chat-area");
+const userInput = document.getElementById("user-input");
+const sendButton = document.getElementById("send-button");
+const teachInput = document.getElementById("teach-input");
+const teachButton = document.getElementById("teach-button");
+const logoutButton = document.getElementById("logout-button");
+const chatgptButton = document.getElementById("chat-gpt-button");
+const feedbackInput = document.getElementById("feedback-input");
+const feedbackButton = document.getElementById("feedback-button");
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+const searchResultsList = document.getElementById("search-results");
+const draggableElement = document.querySelector('.draggable');
+const saveButton = document.getElementById("save-button");
+
+
+let isApiActive = false; // Flag to track API activation status
+let previousUserMessage = ""; // Store the previous user message
+let awaitingTeaching = false; // Flag to track if the chatbot is waiting for teaching
+let isDragging = false;
+let chatHistory = []; // Initialize an empty array to store chat history
+
+teachInput.style.display = "none";
+teachButton.style.display = "none";
+
+chatgptButton.addEventListener("click", toggleApiActivation);
+sendButton.addEventListener("click", sendMessage);
+feedbackButton.addEventListener("click", submitFeedback);
+searchButton.addEventListener("click", searchOnRapidAPI);
+teachButton.addEventListener("click", teachChatbot);
+saveButton.addEventListener("click", saveChatHistory);
+window.addEventListener('load', centerDraggableElement);
+window.addEventListener('resize', centerDraggableElement);
+
+
+draggableElement.addEventListener('mousedown', (e) => {
+    isDragging = true;
+
+
+    const offsetX = e.clientX - draggableElement.getBoundingClientRect().left;
+    const offsetY = e.clientY - draggableElement.getBoundingClientRect().top;
+
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
+
+    function onMouseMove(e) {
+        if (!isDragging) return;
+
+        const newX = e.clientX - offsetX;
+        const newY = e.clientY - offsetY;
+
+        draggableElement.style.left = `${newX}px`;
+        draggableElement.style.top = `${newY}px`;
+    }
+
+    function onMouseUp() {
+        isDragging = false;
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+    }
+});
+
+// Add an event listener for the "keypress" event on the user input field
+teachInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        teachChatbot(event);
+    }
+});
+
+// Add an event listener for the "keypress" event on the user input field
+userInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        sendMessage();
+    }
+});
+
+// Add an event listener for the "keypress" event on the search input field
+searchInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        searchOnRapidAPI();
+    }
+});
+
+// Add an event listener for the "keypress" event on the feedback input field
+feedbackInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        submitFeedback();
+    }
+});
+
+
+function centerDraggableElement() {
+    const draggableElement = document.querySelector('.draggable');
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+
+    const draggableWidth = draggableElement.offsetWidth;
+    const draggableHeight = draggableElement.offsetHeight;
+
+    const newX = (windowWidth - draggableWidth) / 2;
+    const newY = (windowHeight - draggableHeight) / 2;
+
+    draggableElement.style.left = `${newX}px`;
+    draggableElement.style.top = `${newY}px`;
+}
+
+function searchOnRapidAPI() {
+    const searchTerm = searchInput.value.trim();
+    if (searchTerm !== "") {
+        const rapidAPISearchURL = `https://rapidapi.com/search/${encodeURIComponent(searchTerm)}`;
+        window.open(rapidAPISearchURL, '_blank'); // Open in a new tab
+    }
+}
+
+function submitFeedback() {
+    const feedbackInput = document.querySelector('#feedback-input');
+    if (feedbackInput) {
+        const feedback = feedbackInput.value;
+        sendFeedback(feedback);
+    } else {
+        console.error("Feedback input element not found.");
+    }
+}
+
+function sendFeedback(feedback) {
+    // Send the feedback to the backend using fetch API
+    fetch('https://127.0.0.1:5000/submit_feedback', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ feedback: feedback }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.message);
+            // Clear the feedback input after submission
+            document.querySelector('#feedback-input').value = '';
+        })
+        .catch(error => {
+            console.error('Fetch Error:', error);
+        });
+}
+
+function displayErrorMessage(message) {
+    const errorContainer = document.getElementById('error-message');
+    errorContainer.textContent = message;
+    errorContainer.style.display = 'block';
+}
+
+function toggleApiActivation() {
+    isApiActive = !isApiActive; // Toggle the flag
+
+    if (isApiActive) {
+        // Activate API key
+        chatgptButton.textContent = "Deactivate Chat GPT"; // Update button text
+
+        toggleApi(); // Call the toggleApi function
+    } else {
+        // Deactivate API key
+        chatgptButton.textContent = "Activate Chat GPT"; // Update button text
+
+        toggleApi(); // Call the toggleApi function
+    }
+}
+
+function toggleApi() {
+    // Send a request to toggle the API activation status
+    fetch('https://127.0.0.1:5000/toggle_api', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({}),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.activated);
+        })
+        .catch(error => {
+            console.error('Fetch Error:', error);
+        });
+}
+
+function sendMessage() {
+    const userMessage = userInput.value;
+    if (userMessage.trim() !== "") {
+        previousUserMessage = userMessage; // Store the current user message
+        displayMessage(userMessage, "user");
+
+        // Send the user message to the backend using fetch API
+        fetch('https://127.0.0.1:5000/get_response', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ message: userMessage }),
+        })
+            .then(response => response.json())
+            .then(data => {
+                const chatbotResponse = data.response;
+                displayMessage(chatbotResponse, "chatbot");
+
+                // Check if chatbot is asking for teaching
+                if (chatbotResponse.includes("teach")) {
+                    showTeachInputAndButton();
+                } else {
+                    hideTeachInputAndButton();
+                }
+            })
+            .catch(error => {
+                console.error('Fetch Error:', error);
+            });
+
+        userInput.value = "";
+    }
+}
+
+function showTeachInputAndButton() {
+    teachInput.style.display = "block";
+    teachButton.style.display = "block";
+    awaitingTeaching = true;
+}
+
+function hideTeachInputAndButton() {
+    teachInput.style.display = "none";
+    teachButton.style.display = "none";
+    awaitingTeaching = false;
+}
+
+function teachChatbot(event) {
+    if (event) {
+        event.preventDefault(); // Prevent the form submission from refreshing the page
+    }
+    const chatbotResponse = teachInput.value;
+
+    // Send the user message and chatbot response to the backend using fetch API
+    fetch('https://127.0.0.1:5000/teach', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message: previousUserMessage, response: chatbotResponse }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            const teachMessage = data.message;
+            displayMessage(teachMessage, "chatbot");
+            hideTeachInputAndButton();
+        })
+        .catch(error => {
+            console.error('Fetch Error:', error);
+        });
+
+    teachInput.value = "";
+}
+
+function saveChatHistory() {
+    const chatText = chatHistory.map(entry => `[${entry.sender}] ${entry.message}`).join('\n');
+    const blob = new Blob([chatText], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'chat_history.txt';
+    a.click();
+
+    URL.revokeObjectURL(url);
+}
+
+function displayMessage(message, sender) {
+    const messageContainer = document.createElement("div");
+    messageContainer.className = `message-container ${sender}`;
+
+    const avatarImage = document.createElement("img");
+    avatarImage.src = sender === "user" ? "Images/user.png" : "Images/ai.png";
+    avatarImage.alt = sender === "user" ? "User Avatar" : "Chatbot Avatar";
+
+    const messageElement = document.createElement("div");
+    messageElement.className = sender === "user" ? "message-user" : "message-chatbot";
+    messageElement.innerHTML = message;
+
+    // Format and display the message
+    const formattedLines = message.split('\n');
+    const formattedMessage = formattedLines.map(line => `<p class="message-line">${line}</p>`).join('');
+    messageElement.innerHTML = formattedMessage;
+
+    if (sender === "user") {
+        messageContainer.appendChild(messageElement);
+        messageContainer.appendChild(avatarImage);
+    } else {
+        messageContainer.appendChild(avatarImage);
+        messageContainer.appendChild(messageElement);
+    }
+
+    chatArea.appendChild(messageContainer);
+    chatArea.scrollTop = chatArea.scrollHeight; // Auto-scroll to the latest message
+
+    // Store the message in the chat history
+    chatHistory.push({ message, sender });
+
+    chatArea.appendChild(messageContainer);
+    chatArea.scrollTop = chatArea.scrollHeight; // Auto-scroll to the latest message
+}
---
+++ app1.py
@@ -0,0 +1,153 @@
+from flask import Flask, request, jsonify, session
+from flask_cors import CORS
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin
+import openai
+import os
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import logging
+
+
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+api_key_activated = False
+
+app = Flask(__name__)
+
+
+CORS(app, supports_credentials=True, origins=['https://your-ai-ivory.vercel.app'])
+app.config["SECRET_KEY"] = os.urandom(24)
+app.config["SESSION_PERMANENT"] = True
+app.config["SESSION_TYPE"] = "filesystem"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+db = SQLAlchemy(app)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=True)
+    active = db.Column(db.Boolean(), default=True)
+    fs_uniquifier = db.Column(db.String(64), unique=True)
+    roles = db.relationship('Role', secondary='user_roles',
+                            backref=db.backref('users', lazy='dynamic'))
+
+user_roles = db.Table(
+    'user_roles',
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+)
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+
+class TaughtResponse(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_message = db.Column(db.Text, nullable=False)
+    chatbot_response = db.Column(db.Text, nullable=False)
+
+class Feedback(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    feedback_text = db.Column(db.Text, nullable=False)
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+
+@app.route('/get_response', methods=['POST'])
+def get_response():
+    user_message = request.json.get('message', '')
+    received_csrf_token = request.json.get('csrf_token')  # Retrieve CSRF token from the JSON data
+    print("Received user message:", user_message)
+
+    # Validate the CSRF token
+    if received_csrf_token != session.get('csrf_token'):
+        return jsonify({'error': 'Invalid CSRF token'}), 400
+    
+    global api_key_activated
+
+    if not api_key_activated:
+        try:
+            # Query the database for a taught response
+            taught_response = TaughtResponse.query.filter_by(user_message=user_message).first()
+
+            if taught_response:
+                chatbot_response = taught_response.chatbot_response
+                print("Retrieved taught response from database:", chatbot_response)
+                return jsonify({'response': chatbot_response})
+
+            return jsonify({'response': 'I do not know the answer to that. Can you teach me?'})
+        except Exception as e:
+            print("Database query error:", str(e))
+            return jsonify({'error': "An error occurred while retrieving the response."})
+    
+    else:
+        # Use OpenAI to generate a response
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": user_message}
+            ]
+        )
+        chatbot_response = response.choices[0].message['content']
+
+        return jsonify({'response': chatbot_response})
+    
+@app.route('/toggle_api', methods=['POST'])
+def toggle_api():
+    global api_key_activated
+    
+    try:
+        # Toggle the API activation status
+        api_key_activated = not api_key_activated
+        print("API key activation status:", api_key_activated)
+        return jsonify({'activated': api_key_activated})
+    except Exception as e:
+        print("API toggle error:", str(e))
+        return jsonify({'error': "An error occurred while toggling the API activation status."})
+
+@app.route('/teach', methods=['POST']) 
+def teach():
+    user_message = request.json.get('message', '')
+    chatbot_response = request.json.get('response', '')
+
+    try:
+        if user_message and chatbot_response:
+            taught_response = TaughtResponse(user_message=user_message, chatbot_response=chatbot_response)
+            db.session.add(taught_response)
+            db.session.commit()
+
+            print("Teaching successful:", user_message, "->", chatbot_response)
+            return jsonify({'message': "Thanks for teaching me!"})
+        else:
+            raise ValueError("Invalid data provided.")
+    except Exception as e:
+        print("Teaching error:", str(e))
+        return jsonify({'error': "An error occurred while teaching the chatbot."})
+
+@app.route('/submit_feedback', methods=['POST'])
+def submit_feedback():
+    feedback_text = request.json.get('feedback', '')
+
+    if feedback_text:
+        store_feedback(feedback_text)  # Store the feedback in the database
+        return jsonify({'message': "Thank you for your feedback!"})
+    else:
+        return jsonify({'message': "No feedback provided."})
+
+def store_feedback(feedback_text):
+    feedback_entry = Feedback(feedback_text=feedback_text)
+    db.session.add(feedback_entry)
+    db.session.commit()
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True) 
